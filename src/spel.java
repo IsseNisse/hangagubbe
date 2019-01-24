ea8162780093b41910f6067f969c73ee9b1fd3d6
IsseNisse <isak.holmstrom@elev.ga.ntig.se>
2019-01-24 10:20:59 +0100

    Spelet klart

--- src/spel.java
+++ src/spel.java
@@ -7,6 +7,54 @@ import java.util.Scanner;
 public class spel {
     public static void main(String[] args) {
 
+        String correctWord = "test";
+
+        char[] correctWord_array = correctWord.replaceAll("[a-öA-Ö]", "_").toCharArray();
+
+        String[] state = {
+               "  ---| \n" ,
+               "  |  O \n" ,
+               "  | -|-\n" ,
+               "  | /\\ \n" ,
+               " /---\\  \n" ,
+               "/     \\  \n"
+        };
+
+        printWord(correctWord_array);
+        int j = 0;
+        while (j != state.length){
+
+            Scanner tgb = new Scanner(System.in);
+            System.out.println("Gissa på en bokstav");
+            String gissning = tgb.next();
+
+            if (correctWord.contains(gissning)) {
+                for (int i = 0; i < correctWord.length(); i++) {
+                    if (correctWord.charAt(i) == gissning.charAt(0)) {
+                        correctWord_array[i] = gissning.charAt(0);
+                        printWord(correctWord_array);
+                    }
+                }
+            } else {
+                printGubbe(state,j);
+                j++;
+            }
+        }
+    }
+    private static void printGubbe(String[] gubbe, int j) {
+        for (int i = 0; i < j+1; i++) {
+            System.out.print(gubbe[i]);
+        }
+    }
+
+    private static void printWord(char[] correctWord_array) {
+        for (char c : correctWord_array) {
+            System.out.print(c + " ");
+        }
+        System.out.println("\n");
+    }
+
+    private static String getRandomWord() {
         Scanner inputFile;
         try {
             inputFile = new Scanner (new File("input.txt"));
@@ -22,24 +70,6 @@ public class spel {
         Math.random();
         Random R = new Random ();
         int wordIndex = R.nextInt(wordList.size());
-        String correctWord = wordList.get(wordIndex);
-        //String word = correctWord + correctWord.length();
-
-       String correctWord_ = correctWord.replaceAll("[a-öA-Ö]", "_ ");
-       System.out.println(correctWord_);
-       System.out.println(correctWord);
-
-       Scanner tgb = new Scanner(System.in);
-       System.out.println("Gissa på en bokstav");
-       String gissning = tgb.next();
-       for () {
-           if (correctWord.contains(gissning)) {
-               correctWord_.replace
-           } else {
-               System.out.println("Fel!");
-           }
-       }
-
-
+        return wordList.get(wordIndex);
     }
 }
